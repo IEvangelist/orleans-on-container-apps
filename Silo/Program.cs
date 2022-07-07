50416dd13d76ad3ac3e521c9520131af8fa7037c
Brady Gaster <41929050+bradygaster@users.noreply.github.com>
2022-07-07 06:48:23 -0700

    removed dependence on app service (#3)
    the way the code had been written prior, it was specifically looking for app service-injected environment variables that won`t be there if a customer is deploying the app to Azure Container Apps. In that case, one could use configuration settings if one desired, but for now, the two ports - silo and gateway - are presumed to be the defaults. This way it should work on either app service or container apps without modification.

--- Silo/Program.cs
+++ Silo/Program.cs
@@ -13,29 +13,44 @@ await Host.CreateDefaultBuilder(args)
             }
             else
             {
-                var endpointAddress =
-                    IPAddress.Parse(context.Configuration["WEBSITE_PRIVATE_IP"]);
-                var strPorts =
-                    context.Configuration["WEBSITE_PRIVATE_PORTS"].Split(',');
-                if (strPorts.Length < 2)
-                    throw new Exception("Insufficient private ports configured.");
-                var (siloPort, gatewayPort) =
-                    (int.Parse(strPorts[0]), int.Parse(strPorts[1]));
+                var siloPort = 11111;
+                var gatewayPort = 30000;
+
+                // are we running in app service?
+                if (!string.IsNullOrEmpty(context.Configuration["WEBSITE_PRIVATE_IP"]) && !string.IsNullOrEmpty(context.Configuration["WEBSITE_PRIVATE_PORTS"]))
+                {
+                    var endpointAddress =
+                        IPAddress.Parse(context.Configuration["WEBSITE_PRIVATE_IP"]);
+                    var strPorts =
+                        context.Configuration["WEBSITE_PRIVATE_PORTS"].Split(',');
+                    if (strPorts.Length < 2)
+                        throw new Exception("Insufficient private ports configured.");
+                    siloPort = int.Parse(strPorts[0]);
+                    gatewayPort = int.Parse(strPorts[1]);
+
+                    builder
+                        .ConfigureEndpoints(endpointAddress, siloPort, gatewayPort);
+                }
+                else // looks like not, presume we're in Azure Container Apps.
+                {
+                    builder
+                        .ConfigureEndpoints(siloPort, gatewayPort);
+                }
+
                 var connectionString =
                     context.Configuration["ORLEANS_AZURE_STORAGE_CONNECTION_STRING"];
 
                 builder
-                    .ConfigureEndpoints(endpointAddress, siloPort, gatewayPort)
                     .Configure<ClusterOptions>(
                         options =>
                         {
                             options.ClusterId = "ShoppingCartCluster";
                             options.ServiceId = nameof(ShoppingCartService);
-                        }).UseAzureStorageClustering(                    
-                    options => options.ConfigureTableServiceClient(connectionString));
-                builder.AddAzureTableGrainStorage(
-                    "shopping-cart",                    
-                    options => options.ConfigureTableServiceClient(connectionString));
+                        })
+                    .UseAzureStorageClustering(
+                        options => options.ConfigureTableServiceClient(connectionString))
+                    .AddAzureTableGrainStorage("shopping-cart",
+                        options => options.ConfigureTableServiceClient(connectionString));
             }
         })
     .ConfigureWebHostDefaults(
